f04ce28540e29739fc6907ccc3ee931070652533
Ibrahima Sylla <isyll711@gmail.com>
2024-03-18 08:33:04 +0000

    feat: add id to Attachment

--- app/lib/models/attachment.dart
+++ app/lib/models/attachment.dart
@@ -1,8 +1,9 @@
 import 'package:chat_app/types/attachment_type.dart';
 
 class Attachment {
+  final String id;
   final AttachmentType type;
   final int size;
 
-  const Attachment({required this.type, required this.size});
+  const Attachment({required this.id, required this.type, required this.size});
 }
